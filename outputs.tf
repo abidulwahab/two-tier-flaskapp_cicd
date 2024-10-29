e8a46cf59b307c5a8e4fd60f0b16ced91be8774b
abidulwahab <153962308+abidulwahab@users.noreply.github.com>
2024-10-29 10:41:44 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,10 @@ output "control_plane_ip" {
   value = aws_instance.kubeadm_demo_control_plane.public_ip
 }
 
-output "worker_nodes_ip" {
-  value = join("", aws_instance.kubeadm_demo_worker_nodes[*].public_ip)
+output "jenkins_url" {
+  value = "http://${aws_instance.kubeadm_demo_control_plane.public_ip}:8080"
 }
 
+output "worker_nodes_ip" {
+  value = join("\n", aws_instance.kubeadm_demo_worker_nodes[*].public_ip)
+}
